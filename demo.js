bdd5250edabdc25f7dacfeaf1ae10c2267c5194a
Sidpng <siddhartupadhya@gmail.com>
2023-05-15 22:45:15 +0530

    first js file commit

---
+++ demo.js
@@ -0,0 +1,13 @@
+const price1 = 40;
+const price2 = 45;
+const priceArray = [200, 400, 500];
+console.log(priceArray);
+priceArray[1] = 300;
+console.log(priceArray);
+//console.log(price1);
+
+const car = {type:"Fiat", model:"500", color:"white"};
+car.color = 'R';
+car.model = 'P';
+car.type = 'G';
+console.log(car);
